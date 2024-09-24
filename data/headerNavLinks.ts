948e1392383438fd60f9cf1c374b6b30db47e9cb
Jona Schlegel <38097631+jonaschlegel@users.noreply.github.com>
2024-09-24 08:08:48 +0000

    Publish new blog post and adjust image caption styling

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: 'https://jonaschlegel.com/', title: 'Portfolio'},
   { href: '/about', title: 'About' },
 ]
 
