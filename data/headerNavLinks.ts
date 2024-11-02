ea26894d8540c6f7aa49658cc7d8d52641685876
jonaschlegel <jona.schlegel@gmail.com>
2024-11-02 22:09:03 +0100

    Adjust the nav bar

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,8 +2,9 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: 'https://jonaschlegel.com/', title: 'Portfolio'},
+  { href: 'https://jonaschlegel.com/', title: 'Portfolio' },
   { href: '/about', title: 'About' },
+  { href: 'https://www.etsy.com/at/shop/ArchaeoinkDrawings', title: 'Shop' },
 ]
 
 export default headerNavLinks
