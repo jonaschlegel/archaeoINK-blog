44fcdf0de24ecb95d962a31758b87355a90e6eba
jonaschlegel <jona.schlegel@gmail.com>
2024-11-06 14:17:46 +0100

    Change welcome message

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="my-6 flex flex-col items-center gap-x-12 xl:mb-12 xl:flex-row">
         <div className="mr-8 pt-6">
           <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi, and welcome to the archaeoINK!
+            Hi, and welcome to archaeoINK!
           </h1>
           <h2 className="prose text-lg text-gray-600 dark:text-gray-400">
             {`The idea of this blog is to tell and share ${siteMetadata.description}. I am Jona Schlegel: a freelancer, a landscape archaeologist by profession and web developer by training. In my free time, I like to draw and create `}
