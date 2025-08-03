d9ba4ec0d2e0d9ba9bb88f48ab6a0fe80f487ebb
jonaschlegel <jona.schlegel@gmail.com>
2025-08-03 20:04:35 +0200

    Fix eslint error

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,11 +1,11 @@
-import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { genPageMetadata } from 'app/seo'
-import tagData from 'app/tag-data.json'
-import { allBlogs } from 'contentlayer/generated'
-import { slug } from 'github-slugger'
-import type { Metadata } from 'next/types'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import siteMetadata from '@/data/siteMetadata';
+import ListLayout from '@/layouts/ListLayoutWithTags';
+import { genPageMetadata } from 'app/seo';
+import tagData from 'app/tag-data.json';
+import { allBlogs } from 'contentlayer/generated';
+import { slug } from 'github-slugger';
+import type { Metadata } from 'next/types';
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer';
 
 export async function generateMetadata({
   params,
@@ -13,10 +13,9 @@ export async function generateMetadata({
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   params: any
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-}) {
+}): Promise<Metadata> {
   const { tag } = await params
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const decodedTag = (globalThis as any).decodeURI(tag)
+  const decodedTag = decodeURI(tag)
   return genPageMetadata({
     title: decodedTag,
     description: `${siteMetadata.title} ${decodedTag} tagged content`,
@@ -32,10 +31,8 @@ export async function generateMetadata({
 export const generateStaticParams = async () => {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
   const tagCounts = tagData as any
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const tagKeys = (globalThis as any).Object.keys(tagCounts)
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const paths = (tagKeys as any).map((tag: any) => ({
+  const tagKeys = Object.keys(tagCounts)
+  const paths = tagKeys.map((tag) => ({
     tag: tag,
   }))
   return paths
@@ -43,16 +40,13 @@ export const generateStaticParams = async () => {
 
 export default async function TagPage({ params }: { params: any }) {
   const { tag } = await params
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const decodedTag = (globalThis as any).decodeURI(tag)
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  const title =
-    (decodedTag as any)[0].toUpperCase() + (decodedTag as any).split(' ').join('-').slice(1)
+  const decodedTag = decodeURI(tag)
+  const title = decodedTag[0].toUpperCase() + decodedTag.split(' ').join('-').slice(1)
   const filteredPosts = allCoreContent(
     sortPosts(
       /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
       (allBlogs as any).filter(
-        (post: any) => post.tags && (post.tags as any).map((t: any) => slug(t)).includes(decodedTag)
+        (post: any) => post.tags && post.tags.map((t: any) => slug(t)).includes(decodedTag)
       )
     )
   )
